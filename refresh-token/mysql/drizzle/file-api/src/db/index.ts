0de686c3580da47f6d312a23de1e3bfb97ee1693
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-30 09:30:20 +0545

    feat: add Refresh Token schema, configuration, and helper functions for database integration

---
+++ refresh-token/mysql/drizzle/file-api/src/db/index.ts
@@ -0,0 +1,1 @@
+export * from "./schemas/refresh-token.schema";
---
+++ refresh-token/mysql/drizzle/file-api/src/db/schemas/refresh-token.schema.ts
@@ -0,0 +1,40 @@
+import {
+  mysqlTable,
+  varchar,
+  boolean,
+  timestamp,
+  index,
+  text,
+  int
+} from "drizzle-orm/mysql-core";
+import { relations } from "drizzle-orm";
+import { timestamps } from "./schema.helper";
+
+export const refreshTokens = mysqlTable(
+  "refresh_tokens",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    userId: int("user_id")
+      // .references(() => users.id, { onDelete: "cascade" })
+      .notNull(),
+    tokenHash: text("token_hash").notNull(),
+    expiresAt: timestamp("expires_at").notNull(),
+    isRevoked: boolean("is_revoked").default(false).notNull(),
+    revokedAt: timestamp("revoked_at"),
+    replacedByTokenHash: varchar("replaced_by_token_hash", { length: 255 }),
+    ...timestamps
+  },
+  (table) => [
+    index("user_id_idx").on(table.userId),
+    index("token_hash_idx").on(table.tokenHash),
+    index("is_revoked_idx").on(table.isRevoked),
+    index("expires_at_idx").on(table.expiresAt)
+  ]
+);
+
+//TODO: Relations between user and refresh tokens.
+//? Many refresh tokens can be associated with one user.
+//? (Many-to-One)
+
+export type RefreshToken = typeof refreshTokens.$inferSelect;
+export type NewRefreshToken = typeof refreshTokens.$inferInsert;
